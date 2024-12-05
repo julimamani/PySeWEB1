40ec3540ea2bae1c5b1f6b22a61106ab9120ef9c
unknown <juietamamani47@gmail.com>
2024-12-05 23:01:46 +0100

    first commit

---
+++ src/app/components/coordenadas/coordenadas.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-coordenadas',
+  imports: [],
+  templateUrl: './coordenadas.component.html',
+  styleUrl: './coordenadas.component.css'
+})
+export class CoordenadasComponent {
+
+}
