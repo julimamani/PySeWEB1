378dd094407457ecf873e5c1b1e5f1a8ad4bca75
unknown <juietamamani47@gmail.com>
2024-12-09 22:24:52 +0100

    header

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { CoordenadasComponent } from './components/coordenadas/coordenadas.component';
+import path from 'node:path';
+import { TraductorComponent } from './components/traductor/traductor.component';
+export const routes: Routes = [
+    { path:'cg', component: CoordenadasComponent},
+    { path:'traductor', component: TraductorComponent},
 
-export const routes: Routes = [];
+];
---
+++ src/app/components/traductor/traductor.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-traductor',
+  imports: [],
+  templateUrl: './traductor.component.html',
+  styleUrl: './traductor.component.css'
+})
+export class TraductorComponent {
+
+}
